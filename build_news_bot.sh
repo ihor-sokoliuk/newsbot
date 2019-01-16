c9277171eeb5f4125587bc203a7ae8820e58decb
ihor-sokoliuk-exa <ihor.sokoliuk@exabeam.com>
2019-01-16 15:39:00 +0200

    Build script improvements

--- build_news_bot.sh
+++ build_news_bot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+echo
+echo
 program_name="$1"
 telegram_bot_token=$2
 #program_workdir="$GOPATH/src/github.com/ihor-sokoliuk/$program_name"
 #program_workdir_backup="$GOPATH/src/github.com/ihor-sokoliuk/$program_name.backup"
 program_workdir="/opt/telegram_bot/$program_name"
 program_workdir_backup="$program_workdir.backup"
-service_file="/etc/systemd/system/$program_name.service"
+service_name=${program_name}.service
+service_file="/etc/systemd/system/$program_name"
 backup=false
 echo Program name = ${program_name}
 echo Program workdir = ${program_workdir}
@@ -13,64 +16,55 @@ echo Service file = ${service_file}
 
 echo "---=== Stage 0 ===---"
 
-echo Backup project...
+echo "Backup project..."
 if [[ -d ${program_workdir} ]]
 then
 	cp -R ${program_workdir} ${program_workdir_backup}
 	rm -r ${program_workdir}
+	echo "Backup completed."
     backup=true
-fi
-if [[ "$backup" == true ]]
-then
-	echo Backup completed.
 else
-	echo No need in backup.
+    echo "No need in backup."
 fi
 
 echo "---=== Stage 1 ===---"
 
-
-echo Clean project...
 if [[ -e ${service_file} ]]
 then
-	echo Removing service
+	echo "Removing service..."
 	systemctl stop ${program_name}
   	systemctl disable ${program_name}
   	rm -f ${service_file}
   	systemctl daemon-reload
+    echo "Service removed."
+else
+    echo "Skip removing service."
 fi
-echo Clean completed.
 
 echo "---=== Stage 2 ===---"
 
-echo Build project...
-mkdir -p ${program_workdir}
-cp -a . ${program_workdir}
-cd ${program_workdir}
-sed -i 's/{TOKEN}/'${telegram_bot_token}'/g' ${program_name}.yml
-echo Dep ensure...
-go get -u github.com/golang/dep/cmd/dep
-dep ensure
-echo Building...
+echo "Building project..."
 go build -o ${program_name}
 mkdir -p ${program_workdir}
 cp ${program_name} ${program_workdir}
+sed -i 's/{TOKEN}/'${telegram_bot_token}'/g' ${program_name}.yml
 cp ${program_name}.yml ${program_workdir}
 if [[ "$backup" == true ]]
 then
+    echo "Restoring backup database and logs..."
 	cp ${program_workdir_backup}/${program_name}.log ${program_workdir}
 	cp ${program_workdir_backup}/${program_name}.db ${program_workdir}
-	echo Removing backup...
+	echo "Removing backup..."
     rm -r -f ${program_workdir_backup}
 fi
-echo Build complited.
+echo "Build complited."
 
 echo "---=== Stage 3 ===---"
 
-echo Create service...
+echo "Creating service..."
 
 /bin/bash -c "echo '[Unit]
-Description='$program_name'
+Description='$service_name'
 Wants=network-online.target
 After=network-online.target
 
@@ -84,6 +78,6 @@ KillMode=process
 WantedBy=multi-user.target' >> $service_file"
 
 systemctl daemon-reload
-systemctl start ${program_name}
+systemctl start ${service_name}
 
-echo Create completed.
+echo "Service creates"
