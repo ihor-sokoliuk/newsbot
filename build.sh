77aaedd503fed2d14125e1827fced3461f67f5db
ihor-sokoliuk-exa <ihor.sokoliuk@exabeam.com>
2019-01-17 20:14:33 +0200

    Build just tech news bot

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ cd ${project_path}
 echo "Dep ensure..."
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
-./build_news_bot.sh "newsbot" $newsbot_token
+#./build_news_bot.sh "newsbot" $newsbot_token
 ./build_news_bot.sh "technewsbot" $technewsbot_token
