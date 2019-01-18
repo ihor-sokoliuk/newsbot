11447451a6e732174b4257204c1206cc1614c697
ihor-sokoliuk-exa <ihor.sokoliuk@exabeam.com>
2019-01-18 15:07:12 +0200

    Build script improved

--- build_news_bot.sh
+++ build_news_bot.sh
@@ -23,30 +23,18 @@ echo Program name = ${program_name}
 echo Program workdir = ${program_workdir}
 echo Service file = ${service_file}
 
+echo "---=== Stage 1 ===---"
+
+echo "Building project..."
+go build -o ${program_name}
 if [[ ! -f ${program_name} ]]
 then
-    ls -l
     echo "$program_name was not built successfully. Abort!"
     exit 1
 fi
+echo "Build complited."
 
-echo "---=== Stage 0 ===---"
-
-echo "Backup project..."
-if [[ -d ${program_workdir} ]]
-then
-    if [[ "$need_backup" == true ]]
-    then
-        cp -R ${program_workdir} ${program_workdir_backup}
-        echo "Backup completed."
-    fi
-
-	rm -r ${program_workdir}
-else
-    echo "No need in backup."
-fi
-
-echo "---=== Stage 1 ===---"
+echo "---=== Stage 2 ===---"
 
 if [[ -e ${service_file} ]]
 then
@@ -60,25 +48,41 @@ else
     echo "Skip removing service."
 fi
 
-echo "---=== Stage 2 ===---"
+echo "---=== Stage 3 ===---"
 
-echo "Building project..."
-go build -o ${program_name}
+if [[ -d ${program_workdir} ]]
+then
+    if [[ "$need_backup" == true ]]
+    then
+        echo "Backup project..."
+        cp -R ${program_workdir} ${program_workdir_backup}
+        echo "Backup completed."
+    else
+        echo "No need in backup."
+    fi
+
+	rm -r ${program_workdir}
+
+else
+    echo "No need in backup."
+fi
+
+echo "---=== Stage 4 ===---"
+echo "Copying all the files..."
 mkdir -p ${program_workdir}
 cp ${program_name} ${program_workdir}
 sed -i 's/{TOKEN}/'${telegram_bot_token}'/g' ${program_name}.yml
 cp ${program_name}.yml ${program_workdir}
 if [[ "$need_backup" == true ]]
 then
-    echo "Restoring backup database and logs..."
 	cp ${program_workdir_backup}/${program_name}.log ${program_workdir}
 	cp ${program_workdir_backup}/${program_name}.db ${program_workdir}
-	echo "Removing backup..."
     rm -r -f ${program_workdir_backup}
+    echo "Backup of database and logs restored."
 fi
-echo "Build complited."
+echo "Copying all the files done."
 
-echo "---=== Stage 3 ===---"
+echo "---=== Stage 5 ===---"
 
 echo "Creating service..."
 
