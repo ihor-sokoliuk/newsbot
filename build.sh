4880ec1cd4b2880f55c97b31287dc65a9114f6e3
ihor-sokoliuk <yaaappee@gmail.com>
2020-09-28 09:17:13 +0300

    Remove technewsbot

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ echo "Dep ensure..."
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
 ./build_news_bot.sh -p "newsbot" -t $newsbot_token -b true
-./build_news_bot.sh -p "technewsbot" -t $technewsbot_token -b true
